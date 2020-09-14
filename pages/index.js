caa9e16569b63f3203c8dd28c18555da16ec2e3b
Sarah Ransohoff <sranso@gmail.com>
2020-09-13 20:01:55 -0700

    Add homepage styles.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Scone Heads</title>
+        <meta name="description" content="Scone Heads. For scone lovers, by scone lovers. Made by Nan Ransohoff, Sarah Ransohoff, and Sarah Sherman."/>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Yantramanav:wght@100;900&display=swap" rel="stylesheet"/>
       </Head>
@@ -16,29 +17,40 @@ export default function Home() {
           Scone Heads
         </h1>
 
-        <div className={styles.leftBar}>
-          <Link href="/">
-            <a>
-              <h3 className={styles.link}>Art of the Scone</h3>
-            </a>
-          </Link>
+        <div className={styles.linkImageContainer}>
+          <div className={styles.linkWrapper}>
+            <Link href="/">
+              <a>
+                <h3 className={styles.link}>Art of the Scone</h3>
+              </a>
+            </Link>
 
-          <Link href="/">
-            <a>
-              <h3 className={styles.link}>CV</h3>
-            </a>
-          </Link>
+            <Link href="/">
+              <a>
+                <h3 className={styles.link}>CV</h3>
+              </a>
+            </Link>
 
-          <Link href="/">
-            <a>
+            <a href="https://www.instagram.com/scone_heads/" target="_blank">
               <h3 className={styles.link}>Examples</h3>
             </a>
-          </Link>
+
+            <a href="mailto:team@scone-heads.com" target="_blank">
+              <h3 className={styles.link}>Submissions</h3>
+            </a>
+          </div>
+
+          <div className={styles.imageWrapper}>
+            <img src="/scone-hero.jpg"/>
+          </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        Made with buttery love by Nan Ransohoff, Sarah Sherman, and Sarah Ransohoff
+        <p>Made with buttery love by
+          &nbsp;Nan Ransohoff, Sarah Sherman, and&nbsp;
+          <a href="https://sarahransohoff.com" target="_blank">Sarah Ransohoff</a>.
+        </p>
       </footer>
     </div>
   )
